9bb7ebef5dc4feb9a936cc764a64c4d174e90d54
Garrett Berger <77683097+GarrettBWayneState@users.noreply.github.com>
2021-02-11 14:22:38 -0500

    first commit for test project

--- testingGithub/AppDelegate.swift
+++ testingGithub/AppDelegate.swift
@@ -1,13 +1,7 @@
-//
-//  AppDelegate.swift
-//  testingGithub
-//
-//  Created by Server on 2/10/21.
-//
 
 import UIKit
 import CoreData
-
+import Firebase
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -18,10 +12,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
 
-    // MARK: UISceneSession Lifecycle
+   
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
+    
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
